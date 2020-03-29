437fe4215230ecc677b174acca0f4536328349e1
fredericohorst <frederico.horst@gmail.com>
2020-03-29 20:03:04 -0300

    initial commit

---
+++ teste.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+path = 'raw_files/csv/'
+
+# data_2017 = pd.read_csv(path+'developer_survey_2017/survey_results_public.csv')
+# data_2017.head()
+
+data_2011 = pd.read_csv(path+'2012 Stack Overflow Survey Results.csv')
+
+data_2011.head()
+
+
+
+
+
+
